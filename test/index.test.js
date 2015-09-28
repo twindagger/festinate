b37da02caa3abd0b7f31ff85a03a4628421d8892
Colten Rouska <crouska@leisurelink.com>
2015-09-28 16:40:18 -0600

    Pushing up work, adding select statement

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,39 @@
 'use strict';
 
-describe('Festinate', () => {
+let chai = require('chai');
+let expect = chai.expect;
+let Festernate = require('../');
+// let lazy = require('lazy');
 
-  it('should succeed', () => {
-    expect(true).to.be.ok;
+describe('Festinate', () => {
+  let fester;
+  let connection;
+  before(() => {
+    connection = {
+      username: 'redrocket',
+      password: '',
+      server: '',
+      database: 'adventureworks',
+      encrypt: true
+    };
+    fester = new Festernate(connection);
   });
 
+  // it('should succeed', () => {
+  //   expect(true).to.be.ok;
+  // });
+
+  it('connects', (done) => {
+    fester.select("SalesLT.Product", '*')
+      .then(function(cols){
+        // lazy(cols).each(() => {
+        //
+        // });
+        console.log(cols);
+        cols.forEach((col) => {
+          console.log(col.value);
+        });
+        done();
+      }).catch(done);
+  });
 });
